d6819e4c43c9a2c35ea6e7336bc6f930357e1a01
Mykyta Stambul <stambul.n@gmail.com>
2024-08-26 02:43:20 -0700

    add logic to edit profile form adn render cards from template

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,84 @@
 const initialCards = [
   {
     name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    link: "images/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
   {
     name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    link: "images/2-photo-by-ceiline-from-pexels.jpg",
   },
   {
     name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    link: "images/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
   {
     name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    link: "images/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
   {
     name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    link: "images/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
   {
     name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    link: "images/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
 
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
+const editModal = document.querySelector("#edit-modal");
+const projileNameInput = editModal.querySelector("#profile-name-input");
+const profileDescriptionInput = editModal.querySelector(
+  "#profile-description-input"
+);
+
+const editBtn = document.querySelector(".profile__edit-btn");
+const closeBtn = editModal.querySelector(".modal__close-btn");
+editBtn.addEventListener("click", openEditModal);
+closeBtn.addEventListener("click", closeEditModal);
+
+const editForm = editModal.querySelector("#edit-profile-form");
+editForm.addEventListener("submit", handleEditProfileFormSubmit);
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
 function openEditModal() {
-  const modal = document.querySelector(".modal");
-  modal.classList.add("modal_opened");
+  projileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  editModal.classList.add("modal_opened");
 }
 
 function closeEditModal() {
-  const modal = document.querySelector(".modal");
-  modal.classList.remove("modal_opened");
+  editModal.classList.remove("modal_opened");
 }
 
-const editBtn = document.querySelector(".profile__edit-btn");
-editBtn.addEventListener("click", openModal);
+function handleEditProfileFormSubmit(event) {
+  event.preventDefault();
 
-const closeBtn = document.querySelector(".modal__close-btn");
-closeBtn.addEventListener("click", closeModal);
+  profileName.textContent = projileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+
+  closeEditModal();
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardNameEl.textContent = data.name;
+  cardImageEl.setAttribute("src", data.link);
+  cardImageEl.setAttribute("alt", data.name + " image");
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardEl = getCardElement(cardData);
+  cardsList.prepend(cardEl);
+});
